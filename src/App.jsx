10c2da542915b698241b7593998a4021f4aa18fe
Mohit <mohitkhandelwal579@gmail.com>
2024-08-03 12:04:23 +0530

    final2

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import React from 'react';
 import { FiSettings } from 'react-icons/fi';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-function App() {
 
+function App() {
   return (
     <div>
       <BrowserRouter>
-      <div className="flex relative dark:bg-main-dark-bg">
-        <div className="fixed right-4 bottom-4" style={{zIndex:'1000'}}>
-          <TooltipComponent content='Settings' position='Top'>
-          <button type="button" className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' style={{backgroundColor:'blue',borderRadius: '50%'}}>
-            <FiSettings/>
-            
-          </button>
-          </TooltipComponent>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: 1000 }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+                style={{ backgroundColor: 'blue', borderRadius: '50%' }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
         </div>
-      </div>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
